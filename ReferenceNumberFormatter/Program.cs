f3d5499554a660270cf745fb195ef4f73946a626
Pavitra Maduranga <pavitramaduranga@gmail.com>
2024-02-21 09:01:54 +0530

    Add project files.

---
+++ ReferenceNumberFormatter/Program.cs
@@ -0,0 +1,59 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+
+        //Please implement in C# the function that
+        //increments a string based on the rules below.
+        //It should take the string of unknown length and increment the numeric ending of that string by 1.
+        //If numeric ending is overflown it must be reset.
+        //Don’t use regular expressions.
+
+
+        //Function signature:
+        //void Increment(char[] referenceNumber)
+
+
+        //Examples:
+
+        //000002-> 000003
+        //999999-> 000000
+        //999899-> 999900
+        //GL - 321->GL - 322
+        //GL - 999->GL - 000
+        //DRI000EDERS0RE->DRI000EDERS0RE
+        //DRI000EDERS0RE99999->
+        //DRI000EDERS0RE00000
+
+
+        char[] referenceNumber = "GL-999".ToCharArray();
+        Increment(referenceNumber);
+        Console.WriteLine($"GL-999 > {0}", new string(referenceNumber));
+
+    }
+
+    public static void Increment(char[] referenceNumber)
+    {
+        for (int i = referenceNumber.Length - 1; i >= 0; i--)
+        {
+            if (char.IsDigit(referenceNumber[i]))
+            {
+                if (referenceNumber[i] < '9')
+                {
+                    referenceNumber[i]++;
+                    return;
+                }
+                else
+                {
+                    referenceNumber[i] = '0';
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+
+
+}
---
+++ ReferenceNumberFormatterTests/ProgramTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void TestIncrement_000002()
+        {
+            char[] input = "000002".ToCharArray();
+            Program.Increment(input);
+            Assert.AreEqual("000003", new string(input));
+        }
+
+        [TestMethod]
+        public void TestIncrement_999999()
+        {
+            char[] input = "999999".ToCharArray();
+            Program.Increment(input);
+            Assert.AreEqual("000000", new string(input));
+        }
+
+        [TestMethod]
+        public void TestIncrement_999899()
+        {
+            char[] input = "999899".ToCharArray();
+            Program.Increment(input);
+            Assert.AreEqual("999900", new string(input));
+        }
+
+        [TestMethod]
+        public void TestIncrement_GL_321()
+        {
+            char[] input = "GL-321".ToCharArray();
+            Program.Increment(input);
+            Assert.AreEqual("GL-322", new string(input));
+        }
+
+        [TestMethod]
+        public void TestIncrement_GL_999()
+        {
+            char[] input = "GL-999".ToCharArray();
+            Program.Increment(input);
+            Assert.AreEqual("GL-000", new string(input));
+        }
+
+        [TestMethod]
+        public void TestIncrement_DRI000EDERS0RE()
+        {
+            char[] input = "DRI000EDERS0RE".ToCharArray();
+            Program.Increment(input);
+            Assert.AreEqual("DRI000EDERS0RE", new string(input));
+        }
+
+        [TestMethod]
+        public void TestIncrement_DRI000EDERS0RE99999()
+        {
+            char[] input = "DRI000EDERS0RE99999".ToCharArray();
+            Program.Increment(input);
+            Assert.AreEqual("DRI000EDERS0RE00000", new string(input));
+        }
+    }
+}
